487c2110db84405597afa1cefac745db9a7c2cc8
Sebastian <roncancio.juan@correounivalle.edu.co>
2024-05-26 12:22:35 -0500

    register logic implemented, the schemas of the project and conection with database too

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,39 @@
 const express = require('express');
 const app = express();
+const axios = require('axios');
+const cors = require('cors');
+const {ModelUser,ModelTravel,ModelChecklist} = require('./userModel');
+const router = express.Router();
+const dbconnect = require('./config');
+app.use(express.json());
+app.use(cors());
+app.use(router);
+
+// CREAR UN USUARIO ( REGISTRO )
+
+
+router.post("/register", async (req, res) => {
+    try {
+    const { username, email, password } = req.body;
+
+    if (!username || !email || !password) {
+        return res.status(400).json({ message: "Todos los campos son obligatorios." });
+    }
+
+    const newUser = await ModelUser.create({
+        username,
+        email,
+    });
+    res.status(201).json({ message: "Usuario creado con éxito.", user: newUser });
+    } catch (error) {
+    console.error('Error al registrar nuevo usuario:', error);
+    res.status(500).json({ message: "Error al registrar nuevo usuario." });
+    }
+});
 
 app.listen(3001, () => {
     console.log("El servidor esta corriendo en el servidor 3001")
 })
+
+module.exports = app
+dbconnect();
