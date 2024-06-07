99ffa678f4f3e67786b44d7595c996aba55a8f11
Andres Velasco <andresfelipecoronado@gmail.com>
2024-06-06 23:33:41 -0500

    Created the ui and form, missing endpoint

--- frontend/vagabond/src/pages/myTrips/myTrip/FoodandMore.jsx
+++ frontend/vagabond/src/pages/myTrips/myTrip/FoodandMore.jsx
@@ -1,16 +1,26 @@
-import { Box, Container, Typography } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
+import { Box, Container, TextField, Typography } from "@mui/material";
 import { useLocation } from "react-router-dom";
 import Header from "../../../components/Header";
 import HeaderTrip from "../../../components/HeaderTrip";
 import { useLanguageContext } from "../../../context/languageContext";
+import { LoadingButton } from "@mui/lab";
 
 export default function FoodandMore() {
   let { state } = useLocation();
   console.log("State", state);
 
+	const [food, setFood] = useState('');
+	const [description, setDescription] = useState('');
+	const [loading, setLoading] = useState(false);
+
   const { t } = useLanguageContext();
   const texts = (data) => t(`foodandMore.${data}`);
+
+	const handleFood = (e) => {
+		e.preventDefault();
+		console.log("Food", food);
+	}
 
   return (
     <Container component="section" maxWidth="xs">
@@ -41,6 +51,63 @@ export default function FoodandMore() {
             return <Typography variant="body1">{restaurant}</Typography>;
           })}
         </Box>
+				<hr />
+				<Typography variant="h6" fontWeight="bold">
+					{texts('foodPedia')}
+				</Typography>
+
+				<Box component="form" mt={2} onSubmit={handleFood} sx={{display:"flex", gap:1}}>
+					<TextField
+          required
+          label={texts('textField')}
+          variant="outlined"
+          fullWidth
+          value={food}
+          onChange={(e) => setFood(e.target.value)}
+          sx={{
+						height: 55,
+            fontFamily: 'Inter',
+            fontWeight: 400,
+            '& .MuiOutlinedInput-root': {
+              '&.Mui-focused fieldset': {
+                borderColor: '#2D6EFF', // Color del borde al estar seleccionado
+              },
+            },
+            '& input': {
+              fontFamily: 'Inter', 
+            },
+          }}
+        />
+				<LoadingButton loading={loading} type="submit" variant="contained" size="small" sx={{ width: "40%", height:55 }}>
+          {texts("button")}
+        </LoadingButton>
+				</Box>
+				<Box
+          mt={2}
+          sx={{
+            width: "90%",
+            padding: 2,
+            marginInline: "auto",
+            borderStyle: "solid",
+            borderColor: "#e0e0e0",
+            borderWidth: "1px",
+            borderRadius: "10px",
+            color: "#000",
+						minHeight: 100
+          }}
+        >
+					<Typography
+						variant="body1"
+						fontWeight="bold"
+						sx={{ margin: "auto" }}
+					>
+						{food}
+					</Typography>
+					<Typography variant="body2" sx={{ margin: "auto" }}>
+						{description}
+					</Typography>
+				</Box>
+				
       </Box>
     </Container>
   );
